3bb8335d36579d2176cb7688ce283828ee7ded4a
fascinet <mohit26012000@gmail.com>
2021-02-06 23:37:40 +0530

    Change the table  name from migrations to articles and added article.rb file

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ApplicationRecord
+
+#Now it inherit from application record & It has all crud functionalities
+#We don't have to give different names if the class is User then the model name should be user and table name should be users.
+
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_06_144809) do
+ActiveRecord::Schema.define(version: 2021_02_06_150831) do
 
-  create_table "migrations", force: :cascade do |t|
+  create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
