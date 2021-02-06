91a752ade98a9caed7331d1e993570d8960e1e2a
fascinet <mohit26012000@gmail.com>
2021-02-07 01:41:58 +0530

    Add the form and created a option for new data in table

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,20 @@ class ArticlesController < ApplicationController
     #@ will make it a instance variable and now we can use it in the html file
     @article=Article.find(params[:id])
   end
+
+  def index
+    @articles=Article.all
+  end
+
+  def new
+  end
+
+  def create
+    #@article = Article.new(params[:article]) this is not allowed as it also has some secuirty paramaters so we need to whitelash these
+    # to get our required input
+    @article = Article.new(params.require(:article).permit(:title,:description))
+    @article.save
+
+    redirect_to article_path(@article)
+  end
 end
