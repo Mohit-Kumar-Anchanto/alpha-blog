f480fe6bd63621098d9ea4cfca5e104300f730fc
fascinet <mohit26012000@gmail.com>
2021-02-07 00:21:47 +0530

    Added Validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,11 @@
 class Article < ApplicationRecord
 
+  #Validations
+  validates :title ,presence:true , length: {minimum:6, maximum:18}
+  validates :description , presence:true
+
+
+
 #Now it inherit from application record & It has all crud functionalities
 #We don't have to give different names if the class is User then the model name should be user and table name should be users.
 
