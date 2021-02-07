ff5097787188b99213b711c1e11c0eecd779293b
fascinet <mohit26012000@gmail.com>
2021-02-08 01:19:29 +0530

    Added DRY Code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,30 +1,29 @@
 class ArticlesController < ApplicationController
+  before_action :set_action ,only:[:show,:destroy,:edit,:update]
   def show
     #@ will make it a instance variable and now we can use it in the html file
-    @article=Article.find(params[:id])
+    
   end
 
 
   def destroy
-    @article=Article.find(params[:id])
     @article.destroy
     flash[:notice]="Article Deleted Successfully"
     redirect_to articles_path
   
   end
   def edit
-    @article=Article.find(params[:id])
   end
 
   def update
-    @article=Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title,:description))
+    if @article.update(article_params)
       flash[:notice]="Updated successfully"
       redirect_to @article
     else
       render 'edit'
     end
   end
+
   def index
     @articles=Article.all
   end
@@ -36,12 +35,23 @@ class ArticlesController < ApplicationController
   def create
     #@article = Article.new(params[:article]) this is not allowed as it also has some secuirty paramaters so we need to whitelash these
     # to get our required input
-    @article = Article.new(params.require(:article).permit(:title,:description))
+    @article = Article.new(article_params)
     if not @article.save
       render 'new'
     else
       flash[:notice]="Article Saved Successfully"
       redirect_to @article
     end
+  end
+
+
+  private
+
+  def set_action
+    @article=Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title,:description)
   end
 end
