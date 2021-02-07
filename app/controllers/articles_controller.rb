2f301ef9476f74d973e8ef8d488694c26181f9f5
fascinet <mohit26012000@gmail.com>
2021-02-07 23:58:43 +0530

    Added Edit and Update Function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,21 @@ class ArticlesController < ApplicationController
     @article=Article.find(params[:id])
   end
 
+
+
+  def edit
+    @article=Article.find(params[:id])
+  end
+
+  def update
+    @article=Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title,:description))
+      flash[:notice]="Updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
   def index
     @articles=Article.all
   end
