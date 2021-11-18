5a2d8d46d5d582ee0c27a7ce386376483bbd4ef4
Mohit Kumar <mohit.kumar@anchanto.com>
2021-11-18 17:01:43 +0530

    Update application_job.rb

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -2,6 +2,11 @@ class ApplicationJob < ActiveJob::Base
   # Automatically retry jobs that encountered a deadlock
   # retry_on ActiveRecord::Deadlocked
 
+  
+  
+  #github actions 
+  #merge into master and staging
+  
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
 end
